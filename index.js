a8fe5dee861b1fc88e1a0d2c3a99cc49c6cac516
Zak Henry <zak.henry@gmail.com>
2016-05-20 16:42:35 +0100

    feat(sourcemaps): Added hack to force inline sourcemap appending to allow usage with typescript

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var defaultOptions = {
     noAutoWrap: true
 };
 
-module.exports = function(source) {
+module.exports = function(source, sourceMap) {
     var userOptions = loaderUtils.parseQuery(this.query);
     var instrumenter = new istanbul.Instrumenter(
         assign({}, defaultOptions, userOptions)
@@ -19,5 +19,24 @@ module.exports = function(source) {
         this.cacheable();
     }
 
+    // if force sourcemap requested embed inline sources (helps resolve issues with
+    // transpiled languages like typescript
+    if (userOptions['force-sourcemap']) {
+        // if there is no source map, create a dummy one to stop plugins like remap-istanbul complaining
+        if (!sourceMap) {
+            sourceMap = {
+                version: 3,
+                sources: [],
+                sourceRoot: '',
+                names: [],
+                mappings: '',
+                sourcesContent: []
+            };
+        }
+        //encode & append
+        var encoded = new Buffer(JSON.stringify(sourceMap)).toString('base64');
+        source += '\n\n//# sourceMappingURL=data:application/json;base64,' + encoded;
+    }
+    
     return instrumenter.instrumentSync(source, this.resourcePath);
 };
