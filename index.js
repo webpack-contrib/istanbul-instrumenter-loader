6e73149bf18483560d5b4febeee8831445464670
Micha Reiser <micha@famreiser.ch>
2016-09-15 10:09:41 +0200

    Do not set autoWrap to true by default

--- index.js
+++ index.js
@@ -4,14 +4,10 @@ var istanbulLibInstrument = require('istanbul-lib-instrument');
 var loaderUtils = require('loader-utils');
 var assign = require('object-assign');
 
-var defaultOptions = {
-    autoWrap: true
-};
-
 module.exports = function(source, sourceMap) {
     var userOptions = loaderUtils.parseQuery(this.query);
     var instrumenter = istanbulLibInstrument.createInstrumenter(
-        assign({ produceSourceMap: this.sourceMap }, defaultOptions, userOptions)
+        assign({ produceSourceMap: this.sourceMap }, userOptions)
     );
 
     if (this.cacheable) {
