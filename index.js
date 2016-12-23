4d8c130ae69331a8208bec1c3f3e86ba849f9dd3
Sergey Shevchenko <sshevchenko@cogniance.com>
2016-12-23 16:19:40 +0200

    Support inline source maps

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ var istanbulLibInstrument = require('istanbul-lib-instrument');
 var loaderUtils = require('loader-utils');
 var assign = require('object-assign');
 
+var sourceMapRegEx = /(?:\/{2}[#@]{1,2}|\/\*)\s+sourceMappingURL\s*=\s*(data:(?:[^;]+;)+base64,)?(\S+)/;
+
 module.exports = function(source, sourceMap) {
+    // use inline source map, if any    
+    var match = sourceMapRegEx.exec(source);
+    if (!sourceMap && match[1] && match[2]) {
+        sourceMap = JSON.parse(new Buffer(match[2], 'base64').toString('utf8'));
+    }
+
     var userOptions = loaderUtils.parseQuery(this.query);
     var instrumenter = istanbulLibInstrument.createInstrumenter(
         assign({ produceSourceMap: this.sourceMap }, userOptions)
