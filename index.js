6caa1ff500ec1e3c71d146b804afff0f699375b5
Sergey Shevchenko <sshev@users.noreply.github.com>
2016-12-23 18:23:20 +0200

    Support inline source maps (#36)
    :heavy_plus_sign: inline source maps (fix #35)

--- index.js
+++ index.js
@@ -3,8 +3,15 @@
 var istanbulLibInstrument = require('istanbul-lib-instrument');
 var loaderUtils = require('loader-utils');
 var assign = require('object-assign');
+var convert = require('convert-source-map');
 
 module.exports = function(source, sourceMap) {
+    // use inline source map, if any
+    var inlineSourceMap = convert.fromSource(source);
+    if (!sourceMap && inlineSourceMap) {
+        sourceMap = inlineSourceMap.sourcemap;
+    }
+
     var userOptions = loaderUtils.parseQuery(this.query);
     var instrumenter = istanbulLibInstrument.createInstrumenter(
         assign({ produceSourceMap: this.sourceMap }, userOptions)
