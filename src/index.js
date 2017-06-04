7964bff77cc975d3476782aed44423d52c67ba90
Joshua Wiens <joshuaw@easymetrics.com>
2017-06-04 14:14:17 -0500

    fix: Cachable is not nessary, it's the default

--- src/index.js
+++ src/index.js
@@ -3,23 +3,19 @@ import loaderUtils from 'loader-utils';
 import convert from 'convert-source-map';
 
 export default function (source, sourceMap) {
+  let srcMap = sourceMap;
   // use inline source map, if any
   const inlineSourceMap = convert.fromSource(source);
-  if (!sourceMap && inlineSourceMap) {
-    this.sourceMap = inlineSourceMap.sourcemap;
+  if (!srcMap && inlineSourceMap) {
+    srcMap = inlineSourceMap.sourcemap;
   }
 
-  const userOptions = loaderUtils.getOptions(this) || {};
+  const userOptions = loaderUtils.parseQuery(this.query);
   const instrumenter = istanbulLibInstrument.createInstrumenter(
-    Object.assign({ produceSourceMap: this.sourceMap }, userOptions),
+    Object.assign({ produceSourceMap: this.srcMap }, userOptions),
   );
 
-  if (this.cacheable) {
-    this.cacheable();
-  }
-
-  const that = this;
-  return instrumenter.instrument(source, this.resourcePath, (error, src) => {
-    that.callback(error, src, instrumenter.lastSourceMap());
-  }, sourceMap);
+  return instrumenter.instrument(source, this.resourcePath, (error, source) => {
+    this.callback(error, source, instrumenter.lastSourceMap());
+  }, srcMap);
 }
