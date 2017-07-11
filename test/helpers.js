d0cf823972c7e33e2ef918d6472f04882497c9b2
Matt Lewis <mattlewis92@users.noreply.github.com>
2017-07-12 00:55:53 +0100

    test: fix tests on CI (#66)

--- test/helpers.js
+++
@@ -1,4 +0,0 @@
-export function stripLocalPaths(source) { // eslint-disable-line import/prefer-default-export
-  const regexpReplace = new RegExp(__dirname, 'g');
-  return source.replace(regexpReplace, '');
-}
